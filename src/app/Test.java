4c8e163659df6bf3327d7b5d3ac0f98ab90eb583
J-dog12 <jakobjemal@gmail.com>
2022-06-02 13:04:09 -0400

    forth change

--- src/app/Test.java
+++ src/app/Test.java
@@ -2,6 +2,7 @@ package app;
 
 public class Test {
 	public static void main(String[] args) {
+		int fity = 50;
 		System.out.println("Hello World");
 		System.out.println("Hel kmlaflo World");
 		System.out.println("PAPA SMURF");
